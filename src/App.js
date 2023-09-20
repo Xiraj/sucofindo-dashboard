af9b87aecf938acdae33d1f9f0b5fac5c212fa70
Xiraj <akbarsirajuddinh@gmail.com>
2023-09-20 11:27:09 +0700

    create UI Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Main from './pages/Main';
 import Sidebar from './components/Sidebar/Sidebar';
+import Login from './pages/Login';
 
 export default function App () {
   return (
     <div className='flex'>
-      <Sidebar/>
-      <Main/>
+      <Login/>
     </div>
   )
 }
