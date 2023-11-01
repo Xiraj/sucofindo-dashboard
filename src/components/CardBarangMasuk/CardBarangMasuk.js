29a3a0caae54b27993363d84ffd05bfe6c246ccd
Jercarlo <75525322+Jercarlo80@users.noreply.github.com>
2023-11-01 08:20:41 +0700

    responsive card

--- src/components/CardBarangMasuk/CardBarangMasuk.js
+++ src/components/CardBarangMasuk/CardBarangMasuk.js
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box } from "@mui/material";
 import TabBarangMasuk from "../TabBarangMasuk";
 
 export default function CardBarangMasuk () {
-    const [ open ] = useState(true);
-
     return(
-        <Box className={`${open ? 'w-[67.291rem] h-[152vh]':''} bg-white ml-[2.5rem] mt-[3.5rem] rounded-xl`}>
+        <Box className="bg-white w-full md:w-[67.291rem] mx-auto md:h-[94.5rem] mt-12 p-5 rounded-xl">
             <Box className="flex">
                 <h1 className="ml-[2.5rem] pt-[1.5rem] text-[2rem] ">Data Barang Masuk</h1>
             </Box>
--- src/components/CardRiwayat/CardRiwayat.js
+++ src/components/CardRiwayat/CardRiwayat.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Tabs from "../TabRiwayat/TabRiwayat";
 import { Box } from "@mui/material";
 
 export default function CardRiwayat () {
-    const [ open ] = useState(true);
+
     return(
-        <Box className={`${open ? 'w-[67.291rem] h-[195vh]':''} bg-white ml-[2.5rem] mt-[3.5rem] rounded-xl`}>
+        <Box className="bg-white w-full md:w-[67.291rem] mx-auto md:h-[94.5rem] mt-12 p-5 rounded-xl">
             <Box className="flex">
                 <h1 className="ml-[2.5rem] pt-[1.5rem] text-[2rem] ">Riwayat</h1>
             </Box>
