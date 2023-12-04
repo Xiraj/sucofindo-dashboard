5d49609119033774fb090f2143cd3bcf2f6854d4
Jercarlo <75525322+Jercarlo80@users.noreply.github.com>
2023-12-04 10:03:49 +0700

    [FE] Add Page Not Found

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Aset from './Routes/Aset/Aset';
 import Home from './Routes/Home/Home';
 import BarangMasuk from './Routes/BarangMasuk/BarangMasuk';
@@ -8,26 +8,24 @@ import DetailBarangMasuk from './Routes/DetailBarangMasuk';
 import DetailBarangKeluar from './Routes/DetailBarangKeluar';
 import Riwayat from './Routes/Riwayat/Riwayat';
 import Login from './pages/Login/Login';
-import Layout from './components/Layout/Layout';
-import PrivateRoutes from './private/PrivateRoute';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 export default function App () {
   return (
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-        {/* Public Routing */}
-        <Route path='/Login' element={<Login/>}/>
-        {/* Private Routing */}
-        <Route element={<PrivateRoutes/>}>
-          <Route path='/Home' element={<Home/>} exact/>
-          <Route path='/Total-Aset' element={<Aset/>}/>
-          <Route path='/Barang-Masuk' element={<BarangMasuk/>}/>
-          <Route path='/Barang-Keluar' element={<BarangKeluar/>}/>
-          <Route path='/Detail-Barang-Masuk/:_id' element={<DetailBarangMasuk/>}/>
-          <Route path='/Detail-Barang-Keluar/:_id' element={<DetailBarangKeluar/>}/>
-          <Route path='/Riwayat' element={<Riwayat/>}/>
-        </Route>
-      </Route>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/Total-Aset' element={<Aset />} />
+        <Route path='/Barang-Masuk' element={<BarangMasuk />} />
+        <Route path='/Barang-Keluar' element={<BarangKeluar />} />
+        <Route path='/Detail-Barang-Masuk/:_id' element={<DetailBarangMasuk />} />
+        <Route path='/Detail-Barang-Keluar/:_id' element={<DetailBarangKeluar />} />
+        <Route path='/Riwayat' element={<Riwayat />} />
+        {/* Route untuk halaman 404 */}
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </Router>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Aset from './Routes/Aset/Aset';
-import Home from './Routes/Home/Home';
-import BarangMasuk from './Routes/BarangMasuk/BarangMasuk';
-import BarangKeluar from './Routes/BarangKeluar/BarangKeluar';
-import DetailBarangMasuk from './Routes/DetailBarangMasuk';
-import DetailBarangKeluar from './Routes/DetailBarangKeluar';
-import Riwayat from './Routes/Riwayat/Riwayat';
-import Login from './pages/Login/Login';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/Login' element={<Login/>}/>
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/Total-Aset' element={<Aset/>}/>
-      <Route path='/Barang-Masuk' element={<BarangMasuk/>}/>
-      <Route path='/Barang-Keluar' element={<BarangKeluar/>}/>
-      <Route path='/Detail-Barang-Masuk/:_id' element={<DetailBarangMasuk/>}/>
-      <Route path='/Detail-Barang-Keluar/:_id' element={<DetailBarangKeluar/>}/>
-      <Route path='/Riwayat' element={<Riwayat/>}/>
-    </Routes>
-  </Router>
+  <App/>
 );
