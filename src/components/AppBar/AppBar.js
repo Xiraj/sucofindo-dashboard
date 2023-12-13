efdc993671711d63f4aeb592912a3a6ac0e60e70
Jercarlo <75525322+Jercarlo80@users.noreply.github.com>
2023-12-13 09:44:25 +0700

    Update FE

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,148 +1,148 @@
-  import React, { useEffect, useState } from 'react';
-  import AppBar from '@mui/material/AppBar';
-  import Box from '@mui/material/Box';
-  import Toolbar from '@mui/material/Toolbar';
-  import IconButton from '@mui/material/IconButton';
-  import Typography from '@mui/material/Typography';
-  import Menu from '@mui/material/Menu';
-  import Container from '@mui/material/Container';
-  import Tooltip from '@mui/material/Tooltip';
-  import MenuItem from '@mui/material/MenuItem';
-  import { FaRegBell, FaUserCircle } from 'react-icons/fa';
-  import { Link, useNavigate } from 'react-router-dom';
-  import { Badge } from '@mui/material';
-  import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-  import axios from 'axios';
-  import List from '@mui/material/List';
-  import ListItem from '@mui/material/ListItem';
-  import ListItemText from '@mui/material/ListItemText';
-import Notification from '../Notification/Notification';
+import React, { useEffect, useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { FaRegBell, FaUserCircle } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { Badge } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Notification from "../Notification/Notification";
 
+const settings = ["Keluar"];
+const options = [
+  "Show some love to MUI",
+  "Show all notification content",
+  "Hide sensitive notification content",
+  "Hide all notification content",
+];
 
+function UserAppBar() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const [error, setError] = useState(null);
 
-  const settings = ['Keluar'];
-  const options = [
-    'Show some love to MUI',
-    'Show all notification content',
-    'Hide sensitive notification content',
-    'Hide all notification content',
-  ];
+  const navigate = useNavigate();
 
-  function UserAppBar() {
-    const [anchorElNav, setAnchorElNav] = useState(null);
-    const [anchorElUser, setAnchorElUser] = useState(null);
-    const [error, setError] = useState(null);
+  const [name, setName] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://sima-rest-api.vercel.app/api/v1/user/profile", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      })
+      .then((response) => {
+        const username = response.data.username;
+        setName(username);
+      })
+      .catch((error) => {
+        console.error("Error fetching user profile:", error);
+      });
+  }, []);
 
-    const navigate = useNavigate();
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-    const [name, setName] = useState('');
-    useEffect(() => {
-        axios.get('https://sima-rest-api.vercel.app/api/v1/user/profile', {
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make the request to log out
+      const response = await axios.post(
+        "https://sima-rest-api.vercel.app/api/v1/auth/signOut",
+        {},
+        {
           headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
           },
-        })
-        .then(response => {
-          const username = response.data.username;
-          setName(username);
-        })
-        .catch(error => {
-          console.error('Error fetching user profile:', error);
-        });
-      }
-    ,[])
-    
-
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
-
-    const handleLogout = async (e) => {
-      e.preventDefault();
-    
-      try {
-        // Make the request to log out
-        const response = await axios.post(
-          'https://sima-rest-api.vercel.app/api/v1/auth/signOut',
-          {},
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
-            },
-          }
-        );
-    
-        if (response.status === 200) {
-          console.log('Logout successful', response.data);
-    
-          localStorage.clear();
-    
-          navigate('/Login');
-          toast('Logout Berhasil!');
-        } else {
-          console.error('Logout failed', response.data);
-          setError('Logout Gagal');
         }
-      } catch (error) {
-        console.error('Logout error', error);
-        setError('Logout Gagal');
-      }
-    };
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
-    const open = Boolean(anchorEl);
-    const handleClickListItem = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleMenuItemClick = (event, index) => {
-      setSelectedIndex(index);
-      setAnchorEl(null);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
+      );
 
-    return (
-      <>
-      <AppBar position="static" sx={{ backgroundColor: 'white' }}>
+      if (response.status === 200) {
+        console.log("Logout successful", response.data);
+
+        localStorage.clear();
+
+        navigate("/Login");
+        toast("Logout Berhasil!");
+      } else {
+        console.error("Logout failed", response.data);
+        setError("Logout Gagal");
+      }
+    } catch (error) {
+      console.error("Logout error", error);
+      setError("Logout Gagal");
+    }
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const open = Boolean(anchorEl);
+  const handleClickListItem = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <AppBar position="static" sx={{ backgroundColor: "white" }}>
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <Box sx={{ display: 'flex' }}>
-              <Box sx={{ position: 'relative', left: '53.8rem' }}>
-                <Notification/>
+            <Box sx={{ display: "flex" }}>
+              <Box sx={{ position: "relative", left: "53.8rem" }}>
+                <Notification />
               </Box>
-              <Tooltip title="Apakah kamu sudah seleai menggunakan dashboard?">
-                <IconButton onClick={handleOpenUserMenu} sx={{ position: 'relative', left: '53.8rem' }}>
+              <Tooltip title="Apakah kamu sudah selesai menggunakan dashboard?">
+                <IconButton
+                  onClick={handleOpenUserMenu}
+                  sx={{ position: "relative", left: "53.8rem" }}
+                >
                   <h1 className="pr-2 text-[1rem]">{name}</h1>
-                  <FaUserCircle color='#4E73DF' size={30}/>
+                  <FaUserCircle color="#4E73DF" size={30} />
                 </IconButton>
               </Tooltip>
-              
+
               <Menu
-                sx={{ mt: '2.813rem' }}
+                sx={{ mt: "2.813rem" }}
                 id="menu-appbar"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
@@ -159,9 +159,9 @@ import Notification from '../Notification/Notification';
           </Toolbar>
         </Container>
       </AppBar>
-      <ToastContainer/>
-      </>
-    );
-  }
+      <ToastContainer />
+    </>
+  );
+}
 
-  export default UserAppBar;
+export default UserAppBar;
--- src/components/CardBarangKeluar/CardBarangKeluar.js
+++ src/components/CardBarangKeluar/CardBarangKeluar.js
@@ -6,7 +6,7 @@ export default function CardBarangKeluar () {
     const [ open ] = useState(true);
 
     return(
-        <Box className="bg-white w-full md:w-[67.291rem] mx-auto md:h-[94.5rem] mt-12 p-5 rounded-xl">
+        <Box className="bg-white w-full md:w-[67.291rem] mx-auto md:h-[94.5rem] mt-12 p-5 rounded-xl z-0">
             <Box className="flex">
                 <h1 className="ml-[2.5rem] pt-[1.5rem] text-[2rem] ">Data Barang Keluar</h1>
             </Box>
